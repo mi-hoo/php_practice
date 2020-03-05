321572dac033d0b5f5b95ac3364962956823d63d
Miho Asai <mihoo519@gmail.com>
2020-03-05 05:57:06 +0000

    text12_kadai

--- practice.php
+++ practice.php
@@ -34,4 +34,41 @@ $calendar_2018 = [
 
 // 12月を表示する
 echo $calendar_2018["December"];
+
+//　PHP/Laravel 03　課題
+// 課題1
+$name = 'miho asai';
+if ($name == 'miho asai') {
+  echo "私は{$name}です";
+} else {
+  echo "{$name}ではありません";
+}
+
+// 課題2
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+
+// 課題3
+$fruits = array ('banana','apple','grape','peach','berry') ;
+foreach ($fruits as $value) {
+  echo $value;
+}
+
+// 応用課題
+// for文の始めの値を定義する //
+$start = 1;
+// for文の終わりの値を定義する //
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+  }
+}
+
 ?>
